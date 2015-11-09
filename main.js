1c3be8e82a98f21fa074348d1935e24bdd522fde
azharaa <aga.azhar.ahmed@philips.com>
2015-11-09 16:59:42 +0800

    Final Changes with optimised code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * Created by azharaga on 7/11/15.
+ * Created by azharaga on 9/11/15.
  */
 
 var Plotter = require("./plotter"),
@@ -21,97 +21,26 @@ function listToMatrix(list, elementsPerSubArray) {
     return matrix;
 }
 
-var inputMap =
-    [
-        [50,   201,   129   ,947   ,185   ,822   ,298   ,18    ,1027   ,260],
-        [48   ,543   ,104   ,1112  ,1016  ,573   ,833   ,59    ,978    ,1483],
-        [14   ,474   ,767   ,323   ,315   ,667   ,776   ,1398  ,1036   ,567],
-        [103  ,580   ,1134  ,600   ,1161  ,20    ,1242  ,956   ,313    ,600],
-        [317  ,1163  ,339   ,301   ,560   ,608   ,1441  ,285   ,744    ,589],
-        [1266 ,798   ,395   ,78    ,1161  ,1210  ,1077  ,162   ,865    ,25],
-        [78   ,702   ,617   ,889   ,318   ,555   ,1078  ,451   ,922    ,560],
-        [144  ,184   ,4     ,1202  ,8     ,1122  ,1263  ,964   ,1333   ,548],
-        [1094 ,860   ,698   ,374   ,621   ,988   ,347   ,75    ,692    ,1053],
-        [121  ,1159  ,884   ,494   ,668   ,801   ,737   ,1436  ,253    ,757]
-    ];
-
-var rows = 10, columns = 10;
-var input = [];
-
-function readfile() {
-
-    /*var data = fs.readFileSync('./map.txt');
-    var asciiData = data.toString();
-    var splitData = asciiData.split(',');
-
-    var i;
-    for(i = 0; i < splitData.length; i++) {
-        input.push(parseInt(splitData[i]));
-    }
-    console.log(' Total Index ' + i);
-
-
-    console.log(input.length);*/
-
+function make2D() {
     return matrix = listToMatrix(map, 1000);
-
-
 }
-
-
 
 function scanPaths(map) {
     var i, j;
 
     var plot = new Plotter(map, 1000, 1000);
 
-    plot.plotNorth( );
-    plot.plotSouth( );
-    plot.plotEast( );
-    plot.plotWest( );
-    console.log('Computed 2D space');
     plot.plotElevation();
-
-    /*console.log('-------- North ------------');
-    plot.printPaths(northBlock, northBlockValue);
-    console.log('--------------------------');
-
-    console.log('-------- South ------------');
-    plot.printPaths(southBlock, southBlockValue);
-    console.log('--------------------------');
-
-    console.log('-------- East ------------');
-    plot.printPaths(eastBlock, eastBlockValue);
-    console.log('--------------------------');
-
-    console.log('-------- West ------------');
-    plot.printPaths(westBlock, westBlock);
-    console.log('--------------------------');*/
-
-    console.log('Total Elevation ' + plot.elevations.length);
-
-    /*var i;
-        for(i = 0; i < 100; i++) {
-        console.log(i+1 +')-- Computing path for ' + plot.elevations[i].v);
-        plot.computePlausiblePaths(plot.elevations[i], {distance: 1, gradient: 0, path: [plot.elevations[i].v]});
-    }*/
+    console.log('Computed 2D space');
 
     plot.elevations.forEach(function(value, index) {
-
-        console.log(index + ') Computing path for ' + value.v);
-        plot.computePlausiblePaths(value, {distance: 1, gradient: 0, path: [value.v]});
+        if(value.c == false) {
+            plot.computePlausiblePaths(value, {distance: 1, gradient: 0, path: [value.v]});
+        }
     });
     
     var id = plot.findOptimalPath();
-
     console.log('Maximum Distance: ' + id.distance + ' with Gradient: ' + id.gradient);
-
-
-    //plot.printSkiMap();
-
 }
 
-
-scanPaths(readfile(map));
-//scanPaths(inputMap);
-
+scanPaths(make2D(map));
--- plotter.js
+++ plotter.js
@@ -1,5 +1,5 @@
 /**
- * Created by azharaga on 7/11/15.
+ * Created by azharaga on 9/11/15.
  */
 
 function Plotter(map, rows, columns) {
@@ -7,266 +7,140 @@ function Plotter(map, rows, columns) {
     this.rows = rows;
     this.columns = columns;
     this.elevations = [];
-    this.northPaths = [];
-    this.southPaths = [];
-    this.eastPaths = [];
-    this.westPaths = [];
     this.skiMap = [];
 }
 
-Plotter.prototype.plotNorth = function( ) {
-    var i, j;
-    for(j = 0; j < this.columns; j++) {
-        for(i = this.rows -1 ; i >= 1; i--) {
-            if(this.map[i][j] > this.map[i-1][j]) {
-                var path = {x: i, y: j, toX: i-1, toY: j, fromValue: this.map[i][j], toValue: this.map[i-1][j]};
-                this.northPaths.push(path);
-            }
-        }
-    }
-};
-
-Plotter.prototype.plotSouth = function( ) {
-    var i, j;
-    for(j = 0; j < this.columns; j++) {
-        for(i = 0; i < this.rows-1; i++) {
-            if(this.map[i][j] > this.map[i+1][j]) {
-                var path = {x: i, y: j, toX: i+1, toY: j, fromValue: this.map[i][j], toValue: this.map[i+1][j]};
-                this.southPaths.push(path);
-            }
-        }
-    }
-};
-
-Plotter.prototype.plotEast = function( ) {
-    for (i = 0; i < this.rows; i++) {
-        for (j = 0; j < this.columns; j++) {
-            if(this.map[i][j] > this.map[i][j+1]) {
-                var path = {x: i, y: j, toX: i, toY: j+1, fromValue: this.map[i][j], toValue: this.map[i][j+1]};
-                this.eastPaths.push(path);
-            }
-        }
-    }
-};
-
-Plotter.prototype.plotWest = function( ) {
-    for(i = 0; i < this.rows; i++) {
-        for(j = (this.columns - 1) ; j >= 1; j--) {
-            if(this.map[i][j] > this.map[i][j-1]) {
-                var path = {x: i, y: j, toX: i, toY: j-1, fromValue: this.map[i][j], toValue:this.map[i][j-1] };
-                this.westPaths.push(path);
-            }
-        }
-    }
-};
-
 Plotter.prototype.plotElevation = function () {
-    var i, j;
+    var i, j, a = [];
     for(i = 0; i < this.rows; i++) {
         for(j = 0; j < this.columns; j++) {
-            var point = {x: i, y: j, v: this.map[i][j], e: false, w: false, n: false, s: false};
+            var point = {x: i, y: j, v: this.map[i][j], e: false, w: false, n: false, s: false, c: false, i: i+j};
 
             if(((j+1 < this.columns) ) && (this.map[i][j] > this.map[i][j+1])) {
                 point.e = true;
+                var eP = {x: i, y: j+1, v: this.map[i][j+1], i: ((this.columns * i) + (j + 1))};
+                point.eP = eP;
             }
             if((i-1 >= 0) && (this.map[i][j] > this.map[i-1][j])) {
                 point.n = true;
+                var nP = {x: i-1, y: j, v: this.map[i-1][j], i: ((this.columns * (i-1)) + j )}
+                point.nP = nP;
             }
             if((i+1 < this.rows) && this.map[i][j] > this.map[i+1][j]){
                 point.s = true;
+                var sP = {x: i+1, y: j, v: this.map[i+1][j], i: ((this.columns * (i + 1)) + j) }
+                point.sP = sP;
             }
             if((j-1 >= 0) && (this.map[i][j] > this.map[i][j-1])) {
                 point.w = true;
+                var wP = {x: i, y: j-1, v: this.map[i][j-1], i: ((this.columns * i) + (j - 1))}
+                point.wP = wP;
             }
+
             this.elevations.push(point);
         }
     }
-    this.elevations.sort(function(a, b) {
-        return b.v - a.v;
-    });
 };
 
 Plotter.prototype.computePlausiblePaths = function( start, trajectory ) {
     var isLeafNode = true;
+    start.c = true;
 
     if(start.n === true) {
-        var north = this.northPaths.containsPoint(start);
-        if (north != -1) {
-            var distance, gradient, idx;
-            var path = new Array();
+        var north = start.nP;
+        if (north ) {
+            var distance, gradient;
             isLeafNode = false;
-            idx = this.elevations.containsPoint({x: this.northPaths[north].toX, y: this.northPaths[north].toY});
+            var point = this.elevations[north.i];
 
             distance = trajectory.distance;
             gradient = trajectory.gradient;
-            path = trajectory.path.slice(0, trajectory.path.length);
             trajectory.distance++;
-            trajectory.gradient += (this.northPaths[north].fromValue - this.northPaths[north].toValue);
-            trajectory.path.push(this.northPaths[north].toValue);
+            trajectory.gradient += (start.v - point.v);
 
-            this.computePlausiblePaths(this.elevations[idx], trajectory);
+            this.computePlausiblePaths(point, trajectory);
 
             trajectory.distance = distance;
             trajectory.gradient = gradient;
-            trajectory.path = path;
         }
     }
     if(start.s === true) {
-        var south = this.southPaths.containsPoint(start);
+        var south = start.sP;
         if (south != -1) {
-            var distance, gradient, idx;
-            var path = new Array();
+            var distance, gradient;
             isLeafNode = false;
-            idx = this.elevations.containsPoint({x: this.southPaths[south].toX, y: this.southPaths[south].toY});
+            var point  = this.elevations[south.i];
 
             distance = trajectory.distance;
             gradient = trajectory.gradient;
-            path = trajectory.path.slice(0, trajectory.path.length);
-            trajectory.distance++;
-            trajectory.gradient += (this.southPaths[south].fromValue - this.southPaths[south].toValue);
-            trajectory.path.push(this.southPaths[south].toValue);
 
-            this.computePlausiblePaths(this.elevations[idx], trajectory);
+            trajectory.distance++;
+            trajectory.gradient += (start.v - point.v);
+
+            this.computePlausiblePaths(point, trajectory);
 
             trajectory.distance = distance;
             trajectory.gradient = gradient;
-            trajectory.path = path;
         }
     }
 
     if(start.e === true) {
-        var east = this.eastPaths.containsPoint(start);
+        var east = start.eP;
         if (east != -1) {
-            var distance, gradient, idx;
-            var path = new Array();
+            var distance, gradient;
             isLeafNode = false;
-            idx = this.elevations.containsPoint({x: this.eastPaths[east].toX, y: this.eastPaths[east].toY})
+            var point = this.elevations[east.i];
 
             distance = trajectory.distance;
             gradient = trajectory.gradient;
-            path = trajectory.path.slice(0, trajectory.path.length);
-            trajectory.distance++;
-            trajectory.gradient += (this.eastPaths[east].fromValue - this.eastPaths[east].toValue);
-            trajectory.path.push(this.eastPaths[east].toValue);
 
-            this.computePlausiblePaths(this.elevations[idx], trajectory);
+            trajectory.distance++;
+            trajectory.gradient += (start.v - point.v);
+
+            this.computePlausiblePaths(point, trajectory);
 
             trajectory.distance = distance;
             trajectory.gradient = gradient;
-            trajectory.path = path;
         }
     }
 
     if(start.w === true) {
-        var west = this.westPaths.containsPoint(start);
+        var west = start.wP;
         if (west != -1) {
-            var distance, gradient, idx;
-            var path = new Array();
+            var distance, gradient;
             isLeafNode = false;
-            idx = this.elevations.containsPoint( {x: this.westPaths[west].toX, y: this.westPaths[west].toY});
+            var point = this.elevations[west.i];
 
             distance = trajectory.distance;
             gradient = trajectory.gradient;
-            path = trajectory.path.slice(0, trajectory.path.length);
-            trajectory.distance++;
-            trajectory.gradient += (this.westPaths[west].fromValue - this.westPaths[west].toValue);
-            trajectory.path.push(this.westPaths[west].toValue);
 
-            this.computePlausiblePaths(this.elevations[idx], trajectory);
+            trajectory.distance++;
+            trajectory.gradient += (start.v - point.v);
+
+            this.computePlausiblePaths(point, trajectory);
 
             trajectory.distance = distance;
             trajectory.gradient = gradient;
-            trajectory.path = path;
         }
     }
 
     if(isLeafNode === true) {
-        var pushNode = new Object(), paths = new Array();
-
+        var pushNode = new Object();
         pushNode.distance = trajectory.distance;
         pushNode.gradient = trajectory.gradient;
-        pushNode.paths = trajectory.path.slice(0, trajectory.path.length);
+
         this.skiMap.push(pushNode);
-        trajectory.path.length = 0;
     }
 };
 
 Plotter.prototype.findOptimalPath = function() {
   this.skiMap.sort(function(a, b) {
-      return b.paths.length - a.paths.length;
+      return b.distance - a.distance;
   });
-    this.skiMap.forEach(function(value, index) {
-        var str = value.paths.toString(), len = (value.paths.length - 1).toString();
-        str+=  ' (' + len + ')';
-        console.log(str) ;
-    });
-
   return this.skiMap.firstAmongEquals();
-
-};
-
-Plotter.prototype.printPaths = function(paths, pathsValue) {
-    var i;
-    for(i = 0; i < paths.length; i++) {
-        console.log('Path ' + i + ') ' + paths[i].x + ':' + paths[i].y + '->' + paths[i].toX + ':' + paths[i].toY);
-        console.log('From ' + pathsValue[i].fromValue + '->' + pathsValue[i].toValue);
-    }
-};
-
-Plotter.prototype.printSkiMap = function() {
-    var i;
-    for(i = 0; i < this.skiMap.length; i++) {
-        console.log(i+1 + ') Distance:' + this.skiMap[i].distance + ' Gradient: ' + this.skiMap[i].gradient);
-    }
 };
 
 module.exports = Plotter;
-
-if (!Array.prototype.includes) {
-    Array.prototype.includes = function(searchElement ) {
-        'use strict';
-        var O = Object(this);
-        var len = parseInt(O.length) || 0;
-        if (len === 0) {
-            return false;
-        }
-        var n = parseInt(arguments[1]) || 0;
-        var k = 0;
-
-        var currentElement;
-        while (k < len) {
-            currentElement = O[k];
-            if (searchElement === currentElement.v ||
-                (searchElement !== searchElement && currentElement.v !== currentElement.v)) {
-                return true;
-            }
-            k++;
-        }
-        return false;
-    };
-}
-
-if (!Array.prototype.containsPoint) {
-    Array.prototype.containsPoint = function( point ) {
-        'use strict';
-        var O = Object(this);
-        var len = parseInt(O.length) || 0;
-        if (len === 0) {
-            return false;
-        }
-
-        var k = 0;
-        var currentElement;
-        while (k < len) {
-            currentElement = O[k];
-            if((point.x === currentElement.x) && (point.y == currentElement.y)) {
-                return k;
-            }
-            k++;
-        }
-        return -1;
-    };
-}
 
 if (!Array.prototype.firstAmongEquals) {
     Array.prototype.firstAmongEquals = function(  ) {
@@ -295,8 +169,3 @@ if (!Array.prototype.firstAmongEquals) {
         }
     };
 }
-
-
-
-
-
